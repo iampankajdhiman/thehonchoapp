0618a70cf098097288c8b5519c19fd112daba908
iampankajdhiman <pankaj.dhiman@imarkinfotech.com>
2019-09-11 15:19:23 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'splash', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'splash', loadChildren: './splash/splash.module#SplashPageModule' },
 ];
 
 @NgModule({
